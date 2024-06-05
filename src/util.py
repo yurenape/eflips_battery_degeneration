abf94133bc184fcdd277eb9cea2f99da5bc413c9
Ludger Heide <ludger.heide@tu-berlin.de>
2024-06-05 23:51:11 +0200

    Fix documentation maybe?

--- src/util.py
+++ src/util.py
@@ -6,3 +6,13 @@ This module contains utility functions that are used throughout the project.
 def random_number() -> int:
     """Definitely always returns a guaranteed random number"""
     return 4  # chosen by fair dice roll. guaranteed to be random.
+
+
+def no_docu() -> None:
+    # This method has no documentation, in order to lower the pylint score
+    pass
+
+def more_no_docu() -> None:
+    # This method has no documentation, in order to lower the pylint score
+    pass
+
